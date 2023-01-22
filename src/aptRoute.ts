6a420b267cfa753ef1b0031f68515ce4d296d98c
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2023-01-21 22:08:49 -0300

    fix hash

--- src/aptRoute.ts
+++ src/aptRoute.ts
@@ -1,4 +1,4 @@
-import { packagesManeger, packageStorage } from "./packageRegister.js";
+import { packagesManeger } from "./packageRegister.js";
 import { aptSConfig } from "./configManeger.js";
 import coreUtils from "@sirherobrine23/coreutils";
 import express from "express";
@@ -87,46 +87,34 @@ export async function createRouters(package_maneger: packagesManeger, server_con
   });
 
   // Packages stream
-  class PackagesData extends stream.Readable {
-    constructor(packages: packageStorage[], rootPath?: string) {
-      super({read(_size) {}});
-      let breakLine = false;
-      for (const pkg of packages) {
-        const { packageControl, component } = pkg;
-        if (!packageControl.Package) continue;
-        else if (!packageControl.Version) continue;
-        else if (!packageControl.Architecture) continue;
-        else if (!packageControl.Size) continue;
-        if (breakLine) this.push("\n"); else breakLine = true;
-        packageControl.Filename = path.posix.resolve(
-          "/",
-          rootPath ?? "",
-          "pool",
-          component ?? "main",
-          packageControl.Package,
-          packageControl.Architecture,
-          packageControl.Version,
-          "download.deb"
-        );
-        this.push(coreUtils.DebianPackage.createControl(packageControl));
-      }
-      this.push(null);
-    }
+  async function PackagesData(distName: string, componentName: string, packageArch: string, rootFolder?: string): Promise<stream.Readable> {
+    const packagesArray = (await package_maneger.getPackages(distName, componentName)).filter(pkg => (["all", packageArch] as string[]).includes(pkg.packageControl.Architecture));
+    if (!packagesArray.length) return Promise.reject(new Error("Packages not found"));
+    return new stream.Readable({
+      read() {},
+      construct(callback) {
+        if (callback) callback();
+        for (const pkg of packagesArray) {
+          pkg.packageControl.Filename = path.posix.resolve("/", rootFolder ?? "", "pool", pkg.component, pkg.packageControl.Package[0], pkg.packageControl.Package, `${pkg.packageControl.Package}_${pkg.packageControl.Version}_${pkg.packageControl.Architecture}.deb`).slice(1);
+          this.push(coreUtils.DebianPackage.createControl(pkg.packageControl));
+        }
+        this.push(null);
+      },
+    })
   }
   app.get("/dists/:distName/:componentName/binary-:Arch/Packages(.(gz|xz))?", (req, res, next) => {
+    const aptRoot = path.posix.resolve("/", path.posix.join(req.baseUrl, req.path), "../../../../..");
     return Promise.resolve().then(async () => {
-      const packagesArray = (await package_maneger.getPackages(req.params.distName, req.params.componentName)).filter(pkg => (["all", req.params.Arch] as string[]).includes(pkg.packageControl.Architecture));
-      if (!packagesArray.length) return res.status(404).json({ error: "Packages not found" });
-      const packagesStream = new PackagesData(packagesArray);
-      if (req.path.endsWith(".gz")) return packagesStream.pipe(zlib.createGzip()).pipe(res.writeHead(200, {"Content-Type": "application/gzip"}));
-      else if (req.path.endsWith(".xz")) return packagesStream.pipe(lzma.createCompressor()).pipe(res.writeHead(200, {"Content-Type": "application/x-xz"}));
-      return packagesStream.pipe(res.writeHead(200, {"Content-Type": "text/plain"}));
+      const { distName, componentName, Arch } = req.params as any;
+      if (req.path.endsWith(".gz")) return (await PackagesData(distName, componentName, Arch, aptRoot)).pipe(zlib.createGzip()).pipe(res.writeHead(200, {"Content-Type": "application/gzip"}));
+      else if (req.path.endsWith(".xz")) return (await PackagesData(distName, componentName, Arch, aptRoot)).pipe(lzma.createCompressor()).pipe(res.writeHead(200, {"Content-Type": "application/x-xz"}));
+      return (await PackagesData(distName, componentName, Arch, aptRoot)).pipe(res.writeHead(200, {"Content-Type": "text/plain"}));
     }).catch(next);
   });
 
 
   // Release and InRelease
-  async function createRelease(distName: string, returnObject?: boolean) {
+  async function createRelease(distName: string, aptRoot: string, returnObject?: boolean) {
     const { arch, components } = await package_maneger.getDistInfo(distName);
     if (!arch.length) throw new Error("Architectures not found");
     if (!components.length) throw new Error("Components not found");
@@ -165,32 +153,19 @@ export async function createRouters(package_maneger: packagesManeger, server_con
     };
 
     if (pgpKey||aptConfig?.enableHashes) {
-      const allPackagesArray = (await Promise.all(components.map(async componentName => package_maneger.getPackages(distName, componentName)))).flat();
       const data = (await Promise.all(components.map(async componentName => {
         return Promise.all(arch.map(async archName => {
-          const packagesArray = allPackagesArray.filter(pkg => (["all", archName] as string[]).includes(pkg.packageControl.Architecture));
-          const hashs: ReturnType<typeof coreUtils.extendsCrypto.createHashAsync>[] = [];
+          const packagesStream = await PackagesData(distName, componentName, archName);
+          const [rawHash, gzHash, xzHash] = await Promise.all([
+            coreUtils.extendsCrypto.createHashAsync(packagesStream),
+            coreUtils.extendsCrypto.createHashAsync(packagesStream.pipe(zlib.createGzip())),
+            coreUtils.extendsCrypto.createHashAsync(packagesStream.pipe(lzma.createCompressor())),
+          ]);
 
-          let rawSize = 0;
-          const packagesStream = new PackagesData(packagesArray);
-          hashs.push(coreUtils.extendsCrypto.createHashAsync(packagesStream));
-          packagesStream.on("data", chunk => rawSize += chunk.length);
-
-          let gzSize = 0;
-          const gz = packagesStream.pipe(zlib.createGzip());
-          hashs.push(coreUtils.extendsCrypto.createHashAsync(gz));
-          gz.on("data", chunk => gzSize += chunk.length);
-
-          let xzSize = 0;
-          const xz = packagesStream.pipe(lzma.createCompressor());
-          hashs.push(coreUtils.extendsCrypto.createHashAsync(xz));
-          xz.on("data", chunk => xzSize += chunk.length);
-
-          const [rawHash, gzHash, xzHash] = await Promise.all(hashs);
           return {
-            raw: { hash: rawHash, size: rawSize, path: `${componentName}/binary-${archName}/Packages` },
-            gz: { hash: gzHash, size: gzSize, path: `${componentName}/binary-${archName}/Packages.gz` },
-            xz: { hash: xzHash, size: xzSize, path: `${componentName}/binary-${archName}/Packages.xz` }
+            raw: { hash: rawHash.hash, size: rawHash.dataReceived, path: `${componentName}/binary-${archName}/Packages` },
+            gz: { hash: gzHash.hash, size: gzHash.dataReceived, path: `${componentName}/binary-${archName}/Packages.gz` },
+            xz: { hash: xzHash.hash, size: xzHash.dataReceived, path: `${componentName}/binary-${archName}/Packages.xz` }
           }
         }));
       }))).flat(2);
@@ -231,10 +206,14 @@ export async function createRouters(package_maneger: packagesManeger, server_con
 
   // List dists
   app.get("/dists", async (_req, res, next) => package_maneger.getDists().then(dists => res.status(200).json(dists)).catch(next));
-  app.get("/dists/:distName", (req, res, next) => createRelease(req.params.distName, true).then(release => res.status(200).json(release)).catch(next));
+  app.get("/dists/:distName", (req, res, next) => {
+    const aptRoot = path.posix.resolve("/", path.posix.join(req.baseUrl, req.path), "..");
+    return createRelease(req.params.distName, aptRoot, true).then(release => res.status(200).json(release)).catch(next);
+  });
   app.get("/dists/:distName/((InRelease|Release(.gpg)?)?)", (req, res, next) => {
+    const aptRoot = path.posix.resolve("/", path.posix.join(req.baseUrl, req.path), "../..");
     return Promise.resolve().then(async () => {
-      const Release = await createRelease(req.params.distName) as string;
+      const Release = await createRelease(req.params.distName, aptRoot) as string;
       if (req.path.endsWith("InRelease")||req.path.endsWith("Release.gpg")) {
         if (!pgpKey) return res.status(404).json({ error: "Repository not signed" });
         const privateKey = pgpKey.passphrase ? await openpgp.decryptKey({privateKey: await openpgp.readPrivateKey({ armoredKey: pgpKey.privateKey }), passphrase: pgpKey.passphrase}) : await openpgp.readPrivateKey({ armoredKey: pgpKey.privateKey });
