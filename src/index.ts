d491c35bbdf1c4ef090f0eb635186b17125160e2
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2023-04-11 11:29:29 -0300

    Add hex print config and background packages sync

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ import cluster from "node:cluster";
 import aptServer from "./aptServer.js";
 import configManeger from "./configManeger.js";
 import packageManeger from "./packages.js";
+import { aptStreamConfig } from "./config.js";
 
-yargs(process.argv.slice(2)).version(false).help(true).strictCommands().demandCommand().alias("h", "help").command(["server", "serve", "s"], "Run http Server", yargs => yargs.option("config", {
+yargs(process.argv.slice(2)).wrap(process.stdout.getWindowSize?.().at?.(0)||null).version(false).help(true).strictCommands().demandCommand().alias("h", "help").command(["server", "serve", "s"], "Run http Server", yargs => yargs.option("config", {
     string: true,
     alias: "c",
     type: "string",
@@ -32,10 +33,12 @@ yargs(process.argv.slice(2)).version(false).help(true).strictCommands().demandCo
     type: "string",
     alias: "d",
     description: "database url"
-  }).option("debug", {
-    boolean: true,
+  }).option("disable-auto-sync", {
     type: "boolean",
-    default: false
+    boolean: true,
+    alias: "z",
+    default: false,
+    description: "Disable backgroud sync packages"
   }), async options => {
   const pkg = await packageManeger(options.config);
   if (!!options.cluster && options.cluster > 0) pkg.setClusterForks(options.cluster);
@@ -43,22 +46,34 @@ yargs(process.argv.slice(2)).version(false).help(true).strictCommands().demandCo
   if (!!options.port) pkg.setPortListen(options.port);
   if (!!options.db) pkg.setDatabse(options.db);
   let forks = pkg.getClusterForks();
-  if (cluster.isPrimary && (forks > 0)) {
-    const forkProcess = async (count = 0): Promise<number> => new Promise((done, reject) => {
-      const fk = cluster.fork();
-      return fk.on("error", err => {
-        console.error(err);
-        return reject(err);
-      }).on("online", () => done(fk.id)).once("exit", (code, signal) => {
-        count++;
-        if (!signal && code === 0) return console.info("Cluster %s: exited and not restarting", fk.id);
-        else if (count > 5) return console.warn("Cluster get max count retrys!");
-        console.info("Cluster %s: Catch %O, and restating with this is restating count %f", fk.id, code||signal, count);
-        return forkProcess(count);
+  if (cluster.isPrimary) {
+    if (forks > 0) {
+      const forkProcess = async (count = 0): Promise<number> => new Promise((done, reject) => {
+        const fk = cluster.fork();
+        return fk.on("error", err => {
+          console.error(err);
+          return reject(err);
+        }).on("online", () => done(fk.id)).once("exit", (code, signal) => {
+          count++;
+          if (!signal && code === 0) return console.info("Cluster %s: exited and not restarting", fk.id);
+          else if (count > 5) return console.warn("Cluster get max count retrys!");
+          console.info("Cluster %s: Catch %O, and restating with this is restating count %f", fk.id, code||signal, count);
+          return forkProcess(count);
+        });
       });
+      for (let i = 0; i < forks; i++) await forkProcess().then(id => console.info("Cluster %s is online", id));
+    }
+    if (!(options.disableAutoSync ?? options["disable-auto-sync"])) (async () => {
+      while (true) {
+        console.info("Initing package sync!");
+        await pkg.syncRepositorys(() => {});
+        console.log("Next sync after 30 Minutes");
+        await new Promise(done => setTimeout(done, 1800000));
+      }
+    })().catch(err => {
+      console.info("Auto sync packages disabled!");
+      console.error(err);
     });
-    for (let i = 0; i < forks; i++) await forkProcess().then(id => console.info("Cluster %s is online", id));
-    return;
   }
   return aptServer(pkg);
 }).command(["maneger", "m", "$0"], "maneger packages in database", yargs => {
@@ -69,9 +84,12 @@ yargs(process.argv.slice(2)).version(false).help(true).strictCommands().demandCo
     description: "Config file path",
     default: "aptStream.yml",
   }).command(["$0"], "Maneger config", async options => configManeger(await packageManeger(options.parseSync().config))).command(["print", "p"], "Print config to target default is json", yargs => yargs.option("outputType", {
+    description: "target output file, targets ended with '64' is base64 string or 'hex' to hexadecimal",
+    default: "json",
     alias: "o",
-    choices: ["yaml", "yml", "json", "json64", "yaml64", "yml64"],
-    description: "target output file, targets ended with '64' is base64 string",
-    default: "json"
-  }), async (options) => console.log(((await packageManeger(options.config)).toString(options.outputType.startsWith("64") ? "base64" : "utf8", options.outputType.startsWith("json") ? "json" : "yaml"))));
+    choices: [
+      "yaml", "yml", "yaml64", "yml64", "yamlhex", "ymlhex",
+      "json", "json64", "jsonhex",
+    ],
+  }), async (options) => console.log(((new aptStreamConfig(options.config)).toString(options.outputType.endsWith("64") ? "base64": options.outputType.endsWith("hex") ? "hex" : "utf8", options.outputType.startsWith("json") ? "json" : "yaml"))));
 }).parseAsync();
